bd3acb680af25346b3fb267c91564ed9746af082
barchuk_v <barchuk_v@rusklimat.ru>
2025-04-26 15:42:07 +0300

    test

---
+++ oapi-codegen/examples/petstore-expanded/echo/petstore.go
@@ -0,0 +1,50 @@
+// This is an example of implementing the Pet Store from the OpenAPI documentation
+// found at:
+// https://github.com/OAI/OpenAPI-Specification/blob/master/examples/v3.0/petstore.yaml
+//
+// The code under api/petstore/ has been generated from that specification.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	echomiddleware "github.com/labstack/echo/v4/middleware"
+	middleware "github.com/oapi-codegen/echo-middleware"
+	"github.com/oapi-codegen/oapi-codegen/v2/examples/petstore-expanded/echo/api"
+)
+
+func main() {
+	port := flag.String("port", "8080", "Port for test HTTP server")
+	flag.Parse()
+
+	swagger, err := api.GetSwagger()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error loading swagger spec\n: %s", err)
+		os.Exit(1)
+	}
+
+	// Clear out the servers array in the swagger spec, that skips validating
+	// that server names match. We don't know how this thing will be run.
+	swagger.Servers = nil
+
+	// Create an instance of our handler which satisfies the generated interface
+	petStore := api.NewPetStore()
+
+	// This is how you set up a basic Echo router
+	e := echo.New()
+	// Log all requests
+	e.Use(echomiddleware.Logger())
+	// Use our validation middleware to check all requests against the
+	// OpenAPI schema.
+	e.Use(middleware.OapiRequestValidator(swagger))
+
+	// We now register our petStore above as the handler for the interface
+	api.RegisterHandlers(e, petStore)
+
+	// And we serve HTTP until the world ends.
+	e.Logger.Fatal(e.Start(net.JoinHostPort("0.0.0.0", *port)))
+}
---
+++ oapi-codegen/internal/test/client/doc.go
@@ -0,0 +1,3 @@
+package client
+
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --package=client --generate=client,types -o client.gen.go client.yaml
